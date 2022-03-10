Minus sign fixes to the order jacobian and hessian functions. Also improved tests to make the more indicative of issues in the derivatives.

banzai_floyds/orders.py
@@ -63,22 +63,23 @@ def smooth_order_jacobian(theta, x, i, height, k=2):
     # Note we have used the Einstein summation notation
     # weights = w = σ(y - cᵢ Pᵢ(x) + h) σ(-y + cᵢ Pᵢ(x) + h)
     # use σ' = σ (1 - σ)
-    # ∂ⱼw = k Pᵢ(x) σ(y - cᵢ Pᵢ(x) + h) (1 - σ(y - cᵢ Pᵢ(x) + h)) σ(-y + cᵢ Pᵢ(x) + h) + \
-    # - k Pᵢ(x) σ(y - cᵢ Pᵢ(x) + h) σ(-y + cᵢ Pᵢ(x) + h) (1 - σ(-y + cᵢ Pᵢ(x) + h))
-    # ∂ⱼw = k Pᵢ(x) σ(y - cᵢ Pᵢ(x) + h) σ(-y + cᵢ Pᵢ(x) + h) (σ(-y + cᵢ Pᵢ(x) + h) - σ(y - cᵢ Pᵢ(x) + h))
+    # Note the minus signs come from when the polynomials are added or subtracted
+    # ∂ⱼw = -k Pⱼ(x) σ(y - cᵢ Pᵢ(x) + h) (1 - σ(y - cᵢ Pᵢ(x) + h)) σ(-y + cᵢ Pᵢ(x) + h) + \
+    # + k Pⱼ(x) σ(y - cᵢ Pᵢ(x) + h) σ(-y + cᵢ Pᵢ(x) + h) (1 - σ(-y + cᵢ Pᵢ(x) + h))
+    # ∂ⱼw = k Pⱼ(x) σ(y - cᵢ Pᵢ(x) + h) σ(-y + cᵢ Pᵢ(x) + h) (σ(y - cᵢ Pᵢ(x) + h) - σ(-y + cᵢ Pᵢ(x) + h))
     half_height = height // 2 + 0.5
     y_centers = model(x2d)
     polynomial_i = model.basis(i, domain=(np.min(x2d), np.max(x2d)))(x2d)
     sigma_plus = expit(k * (y2d - y_centers + half_height))
     sigma_minus = expit(k * (-y2d + y_centers + half_height))
-    return k * polynomial_i * sigma_minus * sigma_plus * (sigma_minus - sigma_plus)
+    return k * polynomial_i * sigma_minus * sigma_plus * (sigma_plus - sigma_minus)
 
 
 def smooth_order_hessian(theta, x, i, j, height, k=2):
     # σ+ = (y - cᵢ Pᵢ(x) + h)
     # σ- = σ(-y + cᵢ Pᵢ(x) + h)
-    # ∂ᵢ∂ⱼw = k² Pᵢ Pⱼ (σ+ σ- (1 - σ-) (σ- - σ+) + σ- σ+ (1 - σ+) (σ- - σ+) + σ- σ+ (σ- (1 - σ-) - σ+ (1 - σ+)))
-    # ∂ᵢ∂ⱼw = k² Pᵢ Pⱼ (3 σ- - 2 σ-² - 3 σ+ + 2 σ+²)
+    # ∂ᵢ∂ⱼw = k² Pᵢ Pⱼ (σ+ σ- (1 - σ-) (σ+ - σ-) - σ- σ+ (1 - σ+) (σ+ - σ-) - σ- σ+ (σ- (1 - σ-) + σ+ (1 - σ+)))
+    # ∂ᵢ∂ⱼw = k² Pᵢ Pⱼ σ+ σ- ((σ+ - σ-)² +  σ+ (σ+ - 1) + σ- (σ- - 1))
     x2d, y2d = x
     model = Legendre(theta, domain=(np.min(x2d), np.max(x2d)))
     half_height = height // 2 + 0.5
@@ -87,7 +88,8 @@ def smooth_order_hessian(theta, x, i, j, height, k=2):
     sigma_minus = expit(k * (-y2d + y_centers + half_height))
     polynomial_i = model.basis(i, domain=(np.min(x2d), np.max(x2d)))(x2d)
     polynomial_j = model.basis(j, domain=(np.min(x2d), np.max(x2d)))(x2d)
-    hessian = 3 * sigma_minus - 2 * sigma_minus * sigma_minus - 3 * sigma_plus + 2 * sigma_plus * sigma_plus
+    hessian = (sigma_plus - sigma_minus) * (sigma_plus - sigma_minus)
+    hessian += sigma_plus * (sigma_plus - 1) + sigma_minus * (sigma_minus - 1)
     return k * k * polynomial_j * polynomial_i * sigma_plus * sigma_minus * hessian
 
 

banzai_floyds/tests/test_orders.py
@@ -104,9 +104,18 @@ def test_smooth_order_jacobian():
     order_height = 85
     nx, ny = 531, 497
     x = np.meshgrid(np.arange(nx), np.arange(ny))
-    function = lambda *args: smooth_order_weights(*args).sum()
-    derivative = lambda theta, x, height: [smooth_order_jacobian(theta, x, i, height).sum() for i in range(len(theta))]
-    assert check_grad(function, derivative, initial_params, *(x, order_height)) < 1e-3
+    h = 1e-5
+
+    # Check the gradient compared to numerical approximations
+    # f'~= (f(x + h) - f(x - h))/(2 h)
+    for i in range(len(initial_params)):
+        actual_jacobian = smooth_order_jacobian(initial_params, x, i, order_height)
+        step = np.zeros(len(initial_params))
+        step[i] = h
+        numerical_jacobian = smooth_order_weights(initial_params + step, x, order_height)
+        numerical_jacobian -= smooth_order_weights(initial_params - step, x, order_height)
+        numerical_jacobian /= 2.0 * h
+        np.testing.assert_allclose(actual_jacobian, numerical_jacobian, atol=1e-9)
 
 
 def test_smooth_order_hessian():
@@ -114,11 +123,18 @@ def test_smooth_order_hessian():
     order_height = 87
     nx, ny = 521, 495
     x = np.meshgrid(np.arange(nx), np.arange(ny))
+    h = 1e-5
     for i in range(len(initial_params)):
-        function = lambda theta, x, height: smooth_order_jacobian(theta, x, i, height).sum()
-        derivative = lambda theta, x, height: [smooth_order_hessian(theta, x, i, j, height).sum()
-                                               for j in range(len(theta))]
-        assert check_grad(function, derivative, initial_params, *(x, order_height)) < 0.2
+        # Check the gradient compared to numerical approximations
+        # f'~= (f(x + h) - f(x - h))/(2 h)
+        for j in range(len(initial_params)):
+            actual_hessian = smooth_order_hessian(initial_params, x, i, j, order_height)
+            step = np.zeros(len(initial_params))
+            step[j] = h
+            numerical_hessian = smooth_order_jacobian(initial_params + step, x, i, order_height)
+            numerical_hessian -= smooth_order_jacobian(initial_params - step, x, i, order_height)
+            numerical_hessian /= 2.0 * h
+            np.testing.assert_allclose(actual_hessian, numerical_hessian, atol=1e-9)
 
 
 def test_matched_filter_jacobian():
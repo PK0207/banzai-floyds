Fixes to matched filter jacobian

banzai_floyds/matched_filter.py
@@ -84,13 +84,13 @@ def matched_filter_hessian(theta, data, error, weights_function, weights_jacobia
             else:
                 # the hessian of the normalization
                 # ∂ⱼn = Σ(weights * ∂ⱼweights / σ²)/n
-                # ∂ᵢ∂ⱼn = n⁻² (Σ((∂ᵢweights * ∂ⱼweights  + weights * ∂ᵢ∂ⱼweights)/ σ²) * n - ∂ᵢn ∂ⱼn)
+                # ∂ᵢ∂ⱼn = n⁻² (Σ((∂ᵢweights * ∂ⱼweights  + weights * ∂ᵢ∂ⱼweights)/ σ²) * n - n * ∂ᵢn ∂ⱼn)
                 first_term = weights_jacobian_function(theta, x, i, *args)
                 first_term *= weights_jacobian_function(theta, x, j, *args)
                 first_term += weights * weights_hessian[i, j]
                 first_term /= error * error
                 normalization_hessian[i, j] = first_term.sum() * normalization
-                normalization_hessian[i, j] -= normalization_jacobian[i] * normalization_jacobian[j]
+                normalization_hessian[i, j] -= normalization_jacobian[i] * normalization_jacobian[j] * normalization
                 normalization_hessian[i, j] /= normalization * normalization
 
     filter_hessian = np.zeros((theta.size, theta.size))
@@ -101,8 +101,8 @@ def matched_filter_hessian(theta, data, error, weights_function, weights_jacobia
                 filter_hessian[i, j] = filter_hessian[j, i]
                 continue
             # Start with this: (∂ⱼn ∂ᵢs + n ∂ⱼ∂ᵢs -s ∂ⱼ∂ᵢn - ∂ᵢn ∂ⱼs)
-            filter_hessian[i, j] = normalization_jacobian[j] * signal_jacobian[i] + normalization * signal_hessian[j, i]
-            filter_hessian -= signal * normalization_hessian[j, i] - normalization_jacobian[i] * signal_jacobian[j]
+            filter_hessian[i, j] = normalization_jacobian[j] * signal_jacobian[i] + normalization * signal_hessian[i, j]
+            filter_hessian -= signal * normalization_hessian[i, j] + normalization_jacobian[i] * signal_jacobian[j]
             filter_hessian[i, j] *= normalization ** 2.0
             # 2 n ∂ⱼn (n ∂ᵢs - s ∂ᵢn)
             term2 = normalization * signal_jacobian[i] - signal * normalization_jacobian[i]

banzai_floyds/tests/test_orders.py
@@ -7,7 +7,6 @@ from banzai.tests.utils import FakeContext
 from banzai_floyds.frames import FLOYDSObservationFrame
 from banzai.data import CCDData
 from astropy.io import fits
-from scipy.optimize import check_grad
 
 
 def test_blind_center_search():
@@ -142,6 +141,7 @@ def test_matched_filter_jacobian():
     order_height = 85
     nx, ny = 531, 497
     x = np.meshgrid(np.arange(nx), np.arange(ny))
+    h = 1e-5
     np.random.seed(21937415)
     data = np.zeros((ny, nx))
     read_noise = 5  # everything is gain = 1
@@ -152,8 +152,18 @@ def test_matched_filter_jacobian():
     error = np.sqrt(read_noise ** 2.0 + np.abs(data))
 
     args = data, error, smooth_order_weights, smooth_order_jacobian, smooth_order_hessian, x, order_height
-    grad_error = check_grad(matched_filter_metric, matched_filter_jacobian, input_params, *args)
-    assert grad_error < 1e-6
+
+    for i in range(len(input_params)):
+        for param_step in np.arange(input_params[i] - 5, input_params[i] + 5, 0.1):
+            params = input_params.copy()
+            params[i] = param_step
+            actual_jacobian = matched_filter_jacobian(params, *args)
+            derivative_step = np.zeros(len(input_params))
+            derivative_step[i] = h
+            numerical_jacobian = matched_filter_metric(params + derivative_step, *args)
+            numerical_jacobian -= matched_filter_metric(params - derivative_step, *args)
+            numerical_jacobian /= 2.0 * h
+            np.testing.assert_allclose(actual_jacobian[i], numerical_jacobian, atol=2e-7)
 
 
 def test_matched_filter_hessian():
@@ -171,6 +181,22 @@ def test_matched_filter_hessian():
     error = np.sqrt(read_noise ** 2.0 + np.abs(data))
 
     args = data, error, smooth_order_weights, smooth_order_jacobian, smooth_order_hessian, x, order_height
+    h = 1e-5
     for i in range(len(input_params)):
-        assert check_grad(lambda *args: matched_filter_jacobian(*args)[i],
-                          lambda *args: matched_filter_hessian(*args)[i], input_params, *args) < 1e-6
+        # Check the gradient compared to numerical approximations
+        # f'~= (f(x + h) - f(x - h))/(2 h)
+        for j in range(i + 1):
+            numerical_hessians = []
+            actual_hessians = []
+            for param_step in np.arange(input_params[j] - 5, input_params[j] + 5, 1):
+                params = input_params.copy()
+                params[i] = param_step
+                actual_hessian = matched_filter_hessian(params, *args)
+                actual_hessians.append(actual_hessian[i, j])
+                derivative_step = np.zeros(len(input_params))
+                derivative_step[j] = h
+                numerical_hessian = matched_filter_jacobian(params + derivative_step, *args)[i]
+                numerical_hessian -= matched_filter_jacobian(params - derivative_step, *args)[i]
+                numerical_hessian /= 2.0 * h
+                numerical_hessians.append(numerical_hessian)
+            np.testing.assert_allclose(actual_hessians, numerical_hessians, atol=1e-9)
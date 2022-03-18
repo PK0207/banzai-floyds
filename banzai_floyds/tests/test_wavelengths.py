51174495ad80b12556bb3eece06de01007806f6a
Curtis McCully <cmccully@lco.global>
2022-03-18 16:32:09 -0400

    Stubs for the linear part of the wavelength solution.

---
+++ banzai_floyds/tests/test_wavelengths.py
@@ -0,0 +1,9 @@
+def test_1d_metric():
+    # make a random list of lines
+    # simulate a spectrum with some fraction of those lines
+    # Set the dispersion and some minor distortion
+    # add noise to the spectrum
+
+    # Cross correlate the spectrum
+    # Find the linear part of the wavelength solution
+    pass
---
+++ banzai_floyds/wavelengths.py
@@ -0,0 +1,9 @@
+def linear_wavelength_metric(data, error, lines, dispersion, line_width):
+    # build a model using the line list
+    # Return the match filter metric with the model set of lines
+    pass
+
+
+def linear_wavelength_solution(data, error, lines, dispersion, line_width, offset_range):
+    # Step the model spectrum metric through each of the offsets and find the peaks
+    pass

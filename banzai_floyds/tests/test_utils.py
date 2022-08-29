2ab7cb22f96b88cac19d2d8153ab68d0d215aa36
Joseph Chatelain <jchatelain@lco.global>
2022-08-29 11:26:20 -0700

    add some basic plotting utils for testing

---
+++ banzai_floyds/tests/test_utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from astropy.visualization import ZScaleInterval
+from astropy.io import fits
+
+
+def plot_array(data, overlays=None):
+    if len(data) == 2:
+        plt.plot(data[0], data[1])
+    elif len(data.shape) > 1:
+        z_interval = ZScaleInterval().get_limits(data)
+        plt.imshow(data, cmap='gray', vmin=z_interval[0], vmax=z_interval[1])
+        # plt.imshow(data, cmap='gray', vmin=0, vmax=1)
+    else:
+        plt.plot(data)
+    if overlays:
+        for overlay in overlays:
+            plt.plot(overlay[0], overlay[1], color="green")
+    plt.show()
+
+
+def make_fits(data, header=None, filename="test.fits"):
+    hdu = fits.PrimaryHDU(data, header)
+    hdu.writeto(filename)
